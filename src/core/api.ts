cf97f930882cc85138836fecd118fb7118629f66
DongKyu <po4tion0429@gmail.com>
2021-10-11 08:12:53 +0900

    #TypeScript using async & await

--- src/core/api.ts
+++ src/core/api.ts
@@ -9,22 +9,10 @@ export default class Api {
     this.url = url;
   }
 
-  getRequestWithXHR<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
-    this.xhr.open('GET', this.url);
-    this.xhr.addEventListener('load', () => {
-      cb(JSON.parse(this.xhr.response) as AjaxResponse);
-    });
-
-    this.xhr.send();
-  }
-
-  getRequestWithPromise<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
-    fetch(this.url)
-      .then((res) => res.json())
-      .then(cb)
-      .catch(() => {
-        console.error('데이터를 불러오지 못했습니다.');
-      });
+  // 비동기 함수 async & await
+  async request<AjaxResponse>(): Promise<AjaxResponse> {
+    const response = await fetch(this.url);
+    return (await response.json()) as AjaxResponse;
   }
 }
 
@@ -33,12 +21,8 @@ export class NewsFeedApi extends Api {
     super(url);
   }
 
-  getDataWithXHR(cb: (data: NewsFeed[]) => void): void {
-    return this.getRequestWithXHR<NewsFeed[]>(cb);
-  }
-
-  getDataWithPromise(cb: (data: NewsFeed[]) => void): void {
-    return this.getRequestWithPromise<NewsFeed[]>(cb);
+  async getData(): Promise<NewsFeed[]> {
+    return this.request<NewsFeed[]>();
   }
 }
 
@@ -47,11 +31,7 @@ export class NewsDetailApi extends Api {
     super(url);
   }
 
-  getDataWithXHR(cb: (data: NewsDetail) => void): void {
-    return this.getRequestWithXHR<NewsDetail>(cb);
-  }
-
-  getDataWithPromise(cb: (data: NewsDetail) => void): void {
-    return this.getRequestWithPromise<NewsDetail>(cb);
+  getData(): Promise<NewsDetail> {
+    return this.request<NewsDetail>();
   }
 }
